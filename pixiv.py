cda643c41dae52f55f211d21421e3a14a6ad0551
b09780978 <b09780978@gmail.com>
2018-03-22 22:15:20 +0800

    update PixivApi
    1. add exception and handle error.
    2. fix auto close requests.Session.
    3. change method to get rank page.

--- pixiv.py
+++ pixiv.py
@@ -3,11 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+# pixiv url and login url.
 PIXIV = 'https://www.pixiv.net'
 LOGIN_URL = 'https://accounts.pixiv.net/login'
 LOGIN_POST_URL = 'https://accounts.pixiv.net/api/login?lang=zh_tw'
 
+# user-agnet.
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+
+# login data.
 LOGIN_PARAM = { 'lang' : 'zh_tw',
 				'source' : 'pc',
 				'view_type' : 'page',
@@ -25,6 +29,17 @@ LOGIN_POST_DATA = {
 	'return_to' : 'https://www.pixiv.net/',
 }
 
+'''
+	PixivApiException: deal with PixivApi Exception.
+'''
+class PixivApiException(Exception):
+
+	def __init__(self, error_message):
+		self.error_message = error_message
+
+	def __str__(self):
+		return self.error_message
+
 class PixivApi(object):
 	'''
 		set your pixiv_id and password, make you can fetch all image(over 18).
@@ -38,6 +53,32 @@ class PixivApi(object):
 		self.login()
 
 	'''
+		Input:
+			image_url : image's url.
+			file_name : store file name.
+		Output:
+			None, download the image.
+	'''
+	def download(self, image_url, file_name=None):
+		if file_name is None:
+			file_name = image_url.split('/')[-1]
+		else:
+			file_type = image_url.split('.')[-1]
+			file_name_name += '.' + file_type
+
+		response = self.session.get(image_url, stream=True)
+
+		# check whether can download.
+		if response.status_code != 200:
+			raise PixivApiException('Download {} fail, {}.'.format(image_url, response.status_code))
+
+		with open(file_name, 'wb') as f:
+			for chunk in response.iter_content(chunk_size=1024):
+				if chunk:
+					f.write(chunk)
+
+
+	'''
 		login your acount.
 	'''
 	def login(self):
@@ -45,17 +86,24 @@ class PixivApi(object):
 		parser = BeautifulSoup(response.text, 'html.parser')
 		post_key = parser.select('[name=post_key]')[0]['value']
 
+		# prevent to fast.
 		time.sleep(0.5)
 		LOGIN_POST_DATA.update({'pixiv_id' : self.pixiv_id,
 								'password' : self.password,
 								'post_key' : post_key,})
 		self.session.post(LOGIN_POST_URL, data=LOGIN_POST_DATA)
 
+		# use r18 rank to check whether success login.
+		check_login_url = 'https://www.pixiv.net/ranking.php?mode=daily_r18&content=illust'
+		response = self.session.get(check_login_url)
+		if response.status_code != 200:
+			raise PixivApiException('Login fail, {}.'.format(response.status_code))
+
 	'''
 		Input:
 			page : which page that you want to fetch.
 		Output:
-			list of image link.
+			list of image link {'url' : image_url, 'id' : image_id}.
 	'''
 	def get_follow(self, page=1):
 		assert page>0, 'pages must > 0.'
@@ -67,7 +115,7 @@ class PixivApi(object):
 		for block in parser.select('#js-mount-point-latest-following'):
 			data = eval(block['data-items'].replace('null', 'None').replace('true', 'True').replace('false', 'False'))
 			for image_item in data:
-				imagePool.append(image_item['url'].replace('\\',''))
+				imagePool.append( { 'url' : image_item['url'].replace('\\',''), 'id' : image_item['illustId'] })
 
 		return imagePool
 
@@ -76,17 +124,22 @@ class PixivApi(object):
 			author_id : author's pixiv id.
 			page : which page your want to fetch.
 		Output:
-			image link list.
+			image link list {'url' : image_url, 'id' : image_id}.
 	'''
 	def get_author_images(self, author_id, page=1):
 		assert page>0, 'page must > 0.'
 		target_url = 'https://www.pixiv.net/member_illust.php?id={}&type=all&p={}'
 		response = self.session.get(target_url.format(author_id, page))
+
+		# check whether author exits.
+		if response.status_code != 200:
+			raise PixivApiException('Author id {} doesn\'t exist, {}.'.format(author_id, response.status_code))
+
 		parser = BeautifulSoup(response.text, 'html.parser')
 
 		imagePool = []
 		for item in parser.select('._layout-thumbnail'):
-			imagePool.append(item.img['data-src'])
+			imagePool.append( { 'url' : item.img['data-src'], 'id' : item.img['data-id'] })
 
 		return imagePool
 
@@ -95,12 +148,27 @@ class PixivApi(object):
 			images : numbers of image you want to crawl.
 		Output:
 			list content rank male image's link and author's link.
-			[ {'img': image_link, 'author': author_link} ]
+			[ {'url': image_link, 'author': author_link, 'id' : image_id} ]
 	'''
-	def get_rank_male(self, images=10):
+	def get_rank(self, images=10, male=True, daily=False, r18=False):
 		assert images>=0, 'images must >= 0'
-		target_url = 'https://www.pixiv.net/ranking.php?mode=male'
+
+		mode = None
+		# decide whether use daily.
+		if daily:
+			mode = 'daily_r18' if r18 else 'daily'
+		else:
+			mode = 'male' if male else 'female'
+			if r18:
+				mode += '_r18'
+
+		target_url = 'https://www.pixiv.net/ranking.php?mode={}'.format(mode)
 		response = self.session.get(target_url)
+
+		# check whether can get page.
+		if response.status_code != 200:
+			raise PixivApiException('Get rank {} fail, {}.'.format(target_url, response.status_code))
+
 		parser = BeautifulSoup(response.text, 'html.parser')
 
 		counter = 0
@@ -110,41 +178,15 @@ class PixivApi(object):
 				break
 			image_block = block.find_all('div')[1]
 			author_link = '{}{}'.format(PIXIV, image_block.a['href'])
+			image_id = block['data-id']
 			image_link = image_block.div.img['data-src']
-			imagePool.append({'img' : image_link, 'author' : author_link})
-			counter += 1
-
-		return imagePool
-
-	'''
-		Input:
-			images : numbers of image you want to crawl.
-		Output:
-			list content rank r18 image's link and author's link
-			[ {'img': image_link, 'author': author_link} ]
-	'''
-	def get_r18_top(self, images=10):
-		assert images>0, 'images must > 0.'
-		target_url = 'https://www.pixiv.net/ranking.php?mode=daily_r18&content=illust'
-		self.session.get(target_url)
-		response = self.session.get(target_url)
-		parser = BeautifulSoup(response.text, 'html.parser')
-
-		counter = 0
-		imagePool = []
-		for block in parser.select('.ranking-item'):
-			if counter >= images:
-				break
-			image_block = block.find_all('div')[1]
-			author_link = '{}{}'.format(PIXIV, image_block.a['href'])
-			image_link = image_block.div.img['data-src']
-			imagePool.append({'img' : image_link, 'author' : author_link})
+			imagePool.append({'url' : image_link, 'author' : author_link, 'id' : image_id })
 			counter += 1
 
 		return imagePool
 
 	def __del__(self):
-		self.close()
+		self.session.close()
 
 	def close(self):
 		self.session.close()
